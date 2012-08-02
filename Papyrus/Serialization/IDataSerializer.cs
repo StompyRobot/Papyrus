d0590a190da1bd13181673e386c14458f42409cd
Simon Moles <simon.moles@gmail.com>
2012-08-02 20:13:15 +0100

    Added license

--- Papyrus/Serialization/IDataSerializer.cs
+++ Papyrus/Serialization/IDataSerializer.cs
@@ -1,4 +1,11 @@
-﻿namespace Papyrus.Serialization
+﻿/*
+ * Copyright © 2012 Stompy Robot (http://www.stompyrobot.co.uk) (https://github.com/stompyrobot)
+ * 
+ * This program is licensed under the Microsoft Permissive License (Ms-PL). You should
+ * have received a copy of the license along with the source code. If not, an online copy
+ * of the license can be found at https://github.com/stompyrobot/Papyrus/wiki/License.
+ */
+namespace Papyrus.Serialization
 {
 
 	internal interface IDataSerializer
