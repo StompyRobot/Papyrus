a2e9b24e4c1133b34fc04d0ad16ef9b191199340
Simon Moles <simon.moles@gmail.com>
2012-10-07 23:44:28 +0100

    Tidying up

--- Papyrus/Exceptions/DataPointerException.cs
+++ Papyrus/Exceptions/DataPointerException.cs
@@ -1,16 +1,14 @@
-﻿/*
- * Copyright © 2012 Stompy Robot (http://www.stompyrobot.co.uk) (https://github.com/stompyrobot)
+/*
+ * Copyright � 2012 Stompy Robot (http://www.stompyrobot.co.uk) (https://github.com/stompyrobot)
  * 
  * This program is licensed under the Microsoft Permissive License (Ms-PL). You should
  * have received a copy of the license along with the source code. If not, an online copy
  * of the license can be found at https://github.com/stompyrobot/Papyrus/wiki/License.
  */
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace Papyrus
+using System;
+
+namespace Papyrus.Exceptions
 {
 	/// <summary>
 	/// Indicates an error while attempting to resolve a pointer
