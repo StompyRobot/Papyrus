24d35a257cc0a8f945d0d3d0627e7bce220dd4a5
Simon Moles <simon.moles@gmail.com>
2012-10-25 00:48:56 +0100

    Moved converters into main papyrus assembly (required for deserialization)

--- Papyrus.Design/ColorTypeConverter.cs
+++ Papyrus.Design/ColorTypeConverter.cs
@@ -5,6 +5,9 @@
  * have received a copy of the license along with the source code. If not, an online copy
  * of the license can be found at https://github.com/stompyrobot/Papyrus/wiki/License.
  */
+
+#if false
+
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -126,3 +129,5 @@ namespace Papyrus.Design
 		
 	}
 }
+
+#endif
---
+++ Papyrus/Converters/ColorTypeConverter.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * Copyright © 2012 Stompy Robot (http://www.stompyrobot.co.uk) (https://github.com/stompyrobot)
+ * 
+ * This program is licensed under the Microsoft Permissive License (Ms-PL). You should
+ * have received a copy of the license along with the source code. If not, an online copy
+ * of the license can be found at https://github.com/stompyrobot/Papyrus/wiki/License.
+ */
+
+using System;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+
+namespace Papyrus.Converters
+{
+	public class ColorTypeConverter : TypeConverter
+	{
+
+		public override bool CanConvertFrom(ITypeDescriptorContext context, Type sourceType)
+		{
+
+			if (sourceType == typeof(string))
+				return true;
+			
+			return base.CanConvertFrom(context, sourceType);
+		}
+
+		public override bool CanConvertTo(ITypeDescriptorContext context, Type destinationType)
+		{
+
+			if (destinationType == typeof(string))
+				return true;
+
+			return base.CanConvertTo(context, destinationType);
+		}
+
+		public override object ConvertFrom(ITypeDescriptorContext context, CultureInfo culture, object value)
+		{
+
+			if (!(value is string))
+			{
+				return base.ConvertFrom(context, culture, value);
+			}
+
+			var stringValue = (string) value;
+			var values = stringValue.Split(',').ToList().ConvertAll(p => p.Trim());
+
+			if (values.Count != 3 && values.Count != 4)
+				throw new FormatException("Invalid number of params");
+
+			var newColor = new DataTypes.Color();
+			newColor.R = Math.Min(255, Math.Max(int.Parse(values[0]), 0));
+			newColor.G = Math.Min(255, Math.Max(int.Parse(values[1]), 0));
+			newColor.B = Math.Min(255, Math.Max(int.Parse(values[2]), 0));
+
+			if (values.Count == 4)
+				newColor.A = Math.Min(255, Math.Max(int.Parse(values[3]), 0));
+			else
+				newColor.A = 255;
+
+			return newColor;
+
+		}
+
+		public override object ConvertTo(ITypeDescriptorContext context, CultureInfo culture, object value, Type destinationType)
+		{
+
+			var color = value as DataTypes.Color;
+
+			if(destinationType == typeof(string)) {
+
+				return string.Format("{0}, {1}, {2}, {3}", color.R, color.G, color.B, color.A);
+
+			}
+
+			return base.ConvertTo(context, culture, value, destinationType);
+
+		}
+		
+	}
+}
