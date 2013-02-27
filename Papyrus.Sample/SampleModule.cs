0df8d764328d0f15f1bfb1bfc37e7c8a09820112
Simon Moles <simon.moles@gmail.com>
2013-02-27 12:13:26 +0000

    Added license header to all files.

--- Papyrus.Sample/SampleModule.cs
+++ Papyrus.Sample/SampleModule.cs
@@ -1,4 +1,11 @@
-﻿[assembly: Papyrus.PapyrusModule("53BDBC5C-1866-4131-BDA5-70427BE42521", typeof(Papyrus.Sample.SampleModule))]
+﻿/*
+ * Copyright © 2012 Stompy Robot (http://www.stompyrobot.co.uk) (https://github.com/stompyrobot)
+ * 
+ * This program is licensed under the Microsoft Permissive License (Ms-PL). You should
+ * have received a copy of the license along with the source code. If not, an online copy
+ * of the license can be found at https://github.com/stompyrobot/Papyrus/wiki/License.
+ */
+[assembly: Papyrus.PapyrusModule("53BDBC5C-1866-4131-BDA5-70427BE42521", typeof(Papyrus.Sample.SampleModule))]
 
 namespace Papyrus.Sample
 {
--- Papyrus/Exceptions/PluginSaveException.cs
+++ Papyrus/Exceptions/PluginSaveException.cs
@@ -1,3 +1,10 @@
+/*
+ * Copyright © 2012 Stompy Robot (http://www.stompyrobot.co.uk) (https://github.com/stompyrobot)
+ * 
+ * This program is licensed under the Microsoft Permissive License (Ms-PL). You should
+ * have received a copy of the license along with the source code. If not, an online copy
+ * of the license can be found at https://github.com/stompyrobot/Papyrus/wiki/License.
+ */
 using System;
 
 namespace Papyrus.Exceptions
