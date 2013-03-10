81329aa39cf268ccc6e9697ba628c47181bf4e71
Simon Moles <simon.moles@gmail.com>
2013-03-10 22:57:49 +0000

    Assembly version

--- Papyrus/Properties/AssemblyVersion.cs
+++ Papyrus/Properties/AssemblyVersion.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyFileVersion("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersion("1.0.0.0")]
 [assembly: System.Reflection.AssemblyConfiguration("Release")]
 
 
